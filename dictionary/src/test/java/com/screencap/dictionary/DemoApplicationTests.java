6c7c42b8d9a216f891ad8d76806563b075ba726a
James Lee <machingclee@gmail.com>
2020-08-11 02:35:42 +0800

    initial commit

---
+++ dictionary/src/test/java/com/screencap/dictionary/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.screencap.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
